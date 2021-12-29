968ddb3d97e0f0f21332185e40ed9be5d50f60c1
SirSkizo <angel.casanova@estudiante.uam.es>
2021-12-29 13:17:49 +0100

    feat: new parser for download DTO

--- src/main/java/com/trikorasolutions/keycloak/client/dto/KeycloakUserRepresentation.java
+++ src/main/java/com/trikorasolutions/keycloak/client/dto/KeycloakUserRepresentation.java
@@ -2,14 +2,19 @@ package com.trikorasolutions.keycloak.client.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jose4j.json.internal.json_simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.StringJoiner;
 
+/**
+ * This is a Download DTO, that is, it shows only the desired fields when they are requested to KC
+ */
 public class KeycloakUserRepresentation {
 
   @JsonIgnore
@@ -90,25 +95,107 @@ public class KeycloakUserRepresentation {
   }
 
   public static KeycloakUserRepresentation from(JsonObject from) {
+    LOGGER.debug("from(JsonObject)... {}", from);
+    // All response must have a username (exception must be launched in bl)
+    if (from == null || from.getString("username") == null)
+      return null;
 
-    // Cannot reuse code since keycloak response fields have different keys between
-    // admin and user endpoints
-    if (from.containsKey("given_name")) {
-      return new KeycloakUserRepresentation(from.getString("id"), from.getString("given_name"),
-          from.getString("family_name"), from.getString("email"), true,
-          from.getString("preferred_username"));
-    } else if (!from.containsKey(
-        "lastName")) { // Admin user do not have a real name in this version
-      return new KeycloakUserRepresentation(from.getString("id"), "IS_CONFIDENTIAL",
-          "IS_CONFIDENTIAL",
-          from.getString("email"), false, from.getString("username"));
-    } else {
-      return new KeycloakUserRepresentation(from.getString("id"), from.getString("firstName"),
-          from.getString("lastName"), from.getString("email"), from.getBoolean("enabled"),
-          from.getString("username"));
+    // Create the DTO only with the mandatory fields
+    KeycloakUserRepresentation parsedResponse = new KeycloakUserRepresentation(
+        from.getString("username"));
+
+    // Then add only the available optional fields
+    Iterator<String> iterator = from.keySet().iterator();
+    while (iterator.hasNext()) {
+      String key = iterator.next();
+      switch (key) {
+        case "id":
+          parsedResponse.setId(from.getString(key));
+          break;
+        case "given_name":
+        case "firstName":
+          parsedResponse.setFirstName(from.getString(key));
+          break;
+        case "family_name":
+        case "lastName":
+          parsedResponse.setLastName(from.getString(key));
+          break;
+        case "email":
+          parsedResponse.setEmail(from.getString(key));
+          break;
+        case "enabled":
+          parsedResponse.setEnabled(from.getBoolean(key));
+          break;
+        default:
+          break;
+      }
     }
+    //If null then set to false (uses short circuit)
+    parsedResponse.enabled = parsedResponse.enabled != null && parsedResponse.enabled;
+    return parsedResponse;
   }
 
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Boolean getEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(Boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Set<UserDtoCredential> getCredentials() {
+    return credentials;
+  }
+
+  public void setCredentials(Set<UserDtoCredential> credentials) {
+    this.credentials = credentials;
+  }
+
+  /**
+   * Delete if not used in future versions...
+   */
+  @Deprecated
   public static KeycloakUserRepresentation from(JsonArray from) {
     // We only parse one user, so it must be stored in position with index 0
     JsonObject toParse;
@@ -123,5 +210,4 @@ public class KeycloakUserRepresentation {
         toParse.getString("lastName"), toParse.getString("email"), toParse.getBoolean("enabled"),
         toParse.getString("username"));
   }
-
 }
